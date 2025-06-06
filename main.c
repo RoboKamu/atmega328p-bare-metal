43b5973998ff197b8cc95327dabebe1bfcaa5378
Karzan <karzan.maher@gmail.com>
2025-06-06 15:48:30 +0200

    correct blinky port

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include "gpio.h"
 
+
+void delay_ms();
+void delay_us();
+
 int main(){
 	gpio_init(&DDRB, GPIO_MODE_OUT, PORTB5, GPIO_PULLUP_OFF);
 	while(1){
-		gpio_write(&DDRB, PORTB5, 1);
+		gpio_write(&PORTB, PORTB5, 1);
 		for(volatile unsigned int i=0; i<0xFFFF; i++) { /* delay.. */ }
-		gpio_write(&DDRB, PORTB5, 0);
+		gpio_write(&PORTB, PORTB5, 0);
 		for(volatile unsigned int i=0; i<0xFFFF; i++) { /* delay.. */ }		
 	}
 }
