23c590c8f089025de5ed5bd72a534f7b61a8fe93
Karzan <karzan.maher@gmail.com>
2025-06-08 17:44:09 +0200

    update blinky

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "gpio.h"
+#include "delay.h"
 
 int main(){
 	gpio_init(&DDRB, GPIO_MODE_OUT, PORTB5, GPIO_PULLUP_OFF);
+	delay_init();
 	while(1){
-		gpio_write(&PORTB, PORTB5, 1);
-		for(volatile unsigned int i=0; i<0xFFFF; i++) { /* delay.. */ }
-		gpio_write(&PORTB, PORTB5, 0);
-		for(volatile unsigned int i=0; i<0xFFFF; i++) { /* delay.. */ }		
+		// toggle LED every second 
+		gpio_write(&PORTB, PORTB5, !gpio_read(&PORTB, PINB5));
+		delay_ms(1000);
 	}
 }
